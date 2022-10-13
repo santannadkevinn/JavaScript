74dfabcd6a96a06a409f0ed818763b6c2b0e7123
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-10-13 15:20:23 -0300

    trabalhando dados

---
+++ aula 25/dados03.js
@@ -0,0 +1,9 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578;
+console.log(typeof salario);
+
+let estaChovendo = true //or false
+console.log(typeof estaChovendo)
