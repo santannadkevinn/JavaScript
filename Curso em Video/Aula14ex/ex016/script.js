969d8cf91356ab09d4b422189ffcda33392e5680
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-11-03 13:51:43 -0300

    Terminei o modulo E

--- Curso em Video/Aula14ex/ex016/script.js
+++ Curso em Video/Aula14ex/ex016/script.js
@@ -1,13 +1,20 @@
 function contar(){
     let ini = document.getElementById('txti')
-    let fim  = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+    let fim = document.getElementById('txtf')
+    let pas = document.getElementById('txtp')
+    let res = document.getElementById('merda')
     
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
          window.alert('[ERRO] Faltan dados!')
-    }else{
-        res.innerHTML = `'merda'`
+    }else {
+        res.innerHTML = "merda"
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
     }
    
 }
---
+++ Curso em Video/Aula14ex/ex017/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById("seltab")
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um nùmero!')
+    } else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let iten = document.createElement('option')
+            iten.text = `${n} x ${c} = ${n*c}`
+            iten.value = `tab${c}`
+            tab.appendChild(iten)
+            c++
+        }
+    }
+    
+    
+}
