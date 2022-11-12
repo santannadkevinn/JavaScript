f0e9884ca59c5ac394ebc0eb0bbe82d9db08e12d
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-11-11 23:10:10 -0300

    tentando finalizar exercicio

--- Curso em Video/Aula16 ex/modelo/script.js
+++ Curso em Video/Aula16 ex/modelo/script.js
@@ -1,4 +1,44 @@
-let adciona = document.getElementById('num')
+let valor = document.getElementById('fnum')
+let select = document.querySelector('#flista')
 let res = document.getElementById('res')
-let opt = document.getElementById('opt')
-let valores = []
+
+let list = []
+
+function isNumber(n){
+    if (n >= 1 && n <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inList(n, l){
+    if(l.indexOf(n) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adcionar(){
+    if (isNumber(valor.value) && !inList(valor.value, list)){
+        list.push(valor.value)
+        let iten = document.createElement('option')
+        iten.text = `Valor ${valor.value} foi adcionado`
+        select.appendChild(iten)
+
+    }else{
+        window.alert('Numero invalido ou já inserido')
+    }
+    
+    
+}
+
+function result() {
+    let total = document.createElement('p')
+    total.text = `Ao todo, temos <strong>${list.length}</strong> números cadastrados.`
+    res.appendChild(total)
+    let maior = document.createElement('p')
+    maior.text = `O maior valor informado foi ${Math.max(...list)}.`
+    res.appendChild(maior)
+}
--- Udemy/Desafios/Desafio01.js
+++ Udemy/Desafios/Desafio01.js
@@ -1,15 +1,3 @@
-{
-    console.log(1)
-    console.log(3)
-    console.log(5)
-    console.log(7)
-    console.log(9)
-}
-
-{
-    console.log(2)
-    console.log(4)
-    console.log(6)
-    console.log(8)
-    console.log(10)
-}
+let arr = [1,2,3,55,4,14]
+let max = Math.max(...arr)
+console.log(max)
