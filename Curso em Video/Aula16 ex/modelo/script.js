1f9a61199fc084ab0caee114c0adc403d946bdba
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-11-14 00:23:03 -0300

    finalmente acabei o desafio

--- Curso em Video/Aula16 ex/modelo/script.js
+++ Curso em Video/Aula16 ex/modelo/script.js
@@ -22,11 +22,11 @@ function inList(n, l){
 
 function adcionar(){
     if (isNumber(valor.value) && !inList(valor.value, list)){
-        list.push(valor.value)
+        list.push(parseInt(valor.value))
         let iten = document.createElement('option')
         iten.text = `Valor ${valor.value} foi adcionado`
         select.appendChild(iten)
-
+        res.innerText = ''
     }else{
         window.alert('Numero invalido ou já inserido')
     }
@@ -34,11 +34,62 @@ function adcionar(){
     
 }
 
-function result() {
+function result(){
+    //res.innerHTML = typeof(list[0])
+    total(), greater(), less(), sum(), division()
+}
+
+function total() {
     let total = document.createElement('p')
-    total.text = `Ao todo, temos <strong>${list.length}</strong> números cadastrados.`
+    total.setAttribute('id', 'length-value')
+    let totall = document.createTextNode(`Ao todo, temos ${list.length} números cadastrados.`)
+    total.appendChild(totall)
     res.appendChild(total)
-    let maior = document.createElement('p')
-    maior.text = `O maior valor informado foi ${Math.max(...list)}.`
-    res.appendChild(maior)
+
+    
+
+    
+}
+
+function greater(){
+    let greater = document.createElement('p')
+    greater.setAttribute('id', 'greater')
+    let greaterr = document.createTextNode(`O maior valor informado foi ${Math.max(...list)}.`)
+    greater.appendChild(greaterr)
+    res.appendChild(greater)
+}
+
+function less(){
+    let less = document.createElement('p')
+    less.setAttribute('id', 'less')
+    let lessr = document.createTextNode(`O menor valor informado foi ${Math.min(...list)}.`)
+    less.appendChild(lessr)
+    res.appendChild(less)
+}
+
+function sum(){
+    let sum = document.createElement('p')
+    sum.setAttribute('id', 'sum')
+    var su = 0;
+    for(var i =0;i<list.length;i++){
+        su+=list[i];
+    }
+    let summ = document.createTextNode(`Somando todos os valores, temos ${su}`)
+    sum.appendChild(summ)
+    res.appendChild(sum)
+}
+
+function division() {
+    var su = 0;
+    for(var i =0;i<list.length;i++){
+        su+=list[i];
+    }
+    let division = su / list.length
+    let flo = parseFloat(division)
+    
+    let div = document.createElement('p')
+    div.setAttribute('id', 'division')
+    let divv = document.createTextNode(`A média dos valores digitados é ${flo}`)
+    div.appendChild(divv)
+    res.appendChild(div)
 }
--- Udemy/Desafios/Desafio01.js
+++ Udemy/Desafios/Desafio01.js
@@ -1,3 +1,12 @@
-let arr = [1,2,3,55,4,14]
-let max = Math.max(...arr)
-console.log(max)
+
+
+let t = [0,1,2,3,4]
+
+var su = 0;
+    for(var i =0;i<t.length;i++){
+        su+=t[i];
+    }
+console.log(su)
+
+let div = su / t.length
+console.log(div)
