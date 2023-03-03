ccce28400476b40cd9f8086cfe011e0ca5d6fabb
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-03-03 11:49:58 -0300

    ok

---
+++ Udemy/Node/acessarModulos.js
@@ -0,0 +1,2 @@
+const modulaA =  require('./moduloA') 
+console.log(modulaA.ola)
---
+++ Udemy/Node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Fala Pessoal'
+exports.benVindo = 'Bem vindo ao node'
+module.exports.ateLogo = 'Até o proximo Exemplo'
