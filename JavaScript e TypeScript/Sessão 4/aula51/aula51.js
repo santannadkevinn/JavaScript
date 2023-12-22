16994672b8edb717d3d0e9eb2b8134fc9dee351e
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-12-22 09:51:06 -0300

    aula51

--- JavaScript e TypeScript/Sessão 4/aula51/aula51.js
+++ JavaScript e TypeScript/Sessão 4/aula51/aula51.js
@@ -1,3 +1,13 @@
-document.addEventListener('click',function(){
-    document.body.style.backgroundColor = 'red'
-})
+function criarMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador
+    }
+}
+
+const duplica = criarMultiplicador(2)
+const triplica = criarMultiplicador(3)
+const quadriplica = criarMultiplicador(4)
+
+console.log(duplica(3))
+console.log(triplica(2))
+console.log(quadriplica(2))
