e15c4854c7d591688f71213c4132d7d0013c8762
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-12-25 19:47:02 -0300

    aula61

--- JavaScript e TypeScript/Sessão 4/aula61/script.js
+++ JavaScript e TypeScript/Sessão 4/aula61/script.js
@@ -1,7 +1,18 @@
-function recursiva(max) {
-  if (max >=10) return;
-  max ++
-  console.log(max)
-  recursiva(max)
+function* geradora1() {
+  //codigo....
+  yield 'valor 1';
+  //codigo...
+  yield 'valor 2'
+  //codigo
+  yield 'valor 3'
 }
-recursiva(0)
+
+function* geradora2() {
+  let i = 0
+
+  while(true) {
+    yield i
+    i++ 
+  }
+}
+
