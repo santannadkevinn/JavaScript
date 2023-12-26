21a4d1297238abc653dac38c17bebaf68246c074
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-12-26 10:18:36 -0300

    aula63

---
+++ JavaScript e TypeScript/Sessão 5/aula63/main.js
@@ -0,0 +1,7 @@
+const nome = ['maria', 'joão', 'eduardo', 'gabriel', 'julia'];
+
+nome.splice(2, 0, 'luiz', 'otavio');
+console.log(nome)
+
+//nome.splice(indice atual, delete, elemento1, elemento2, elemento3)
+
