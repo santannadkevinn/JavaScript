1f7ae464fc7bfb595c737428355d995becec3012
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-12-28 10:19:42 -0300

    aula76

--- JavaScript e TypeScript/Sessão 6/aula76/main.js
+++ JavaScript e TypeScript/Sessão 6/aula76/main.js
@@ -13,5 +13,36 @@ function Produto(nome, preco) {
  function Camiseta(nome, preco, cor) {
     Produto.call(this, nome, preco);
  }
+Camiseta.prototype = Object.create(Produto.prototype)
+Camiseta.prototype.constructor = Camiseta
 
- const camiseta  = new Camiseta()
+Camiseta.prototype.aumento = (percentual) => {
+   this.preco = this.preco + (this.preco *(percentual / 100))
+}
+
+function Caneca(nome, preco, material, estoque) {
+   Produto.call(this, nome, preco);
+   this.material = material;
+   
+   Object.defineProperty(this, 'estoque', {
+      Enumerator: true,
+      configurable: false,
+      get: function() {
+         return estoque
+      },
+      set: function(valor) {
+         if(typeof valor !== 'number') return;
+         estoque = valor
+      }
+   })
+}
+
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca
+
+const caneca = new Caneca('caneca', 15, 'ceramica', 10)
+
+
+
+
+ console.log(Caneca)
